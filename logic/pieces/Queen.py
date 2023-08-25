ae50a562876bdc27867fcd15429519b39b38db11
VuJaxon <vu.tran10100305@gmail.com>
2023-08-25 17:15:37 +0700

    add Queen moving logic

--- logic/pieces/Queen.py
+++ logic/pieces/Queen.py
@@ -1,83 +1,123 @@
 import pygame
-
-from data.classes.Piece import Piece
+from logic.attributes import Piece, GameState
+from typing import Tuple
 
 class Queen(Piece):
-	def __init__(self, pos, color, board):
-		super().__init__(pos, color, board)
+  def __init__(self, pos, color, board):
+    super().__init__(pos, color, board)
 
-		img_path = 'data/imgs/' + color[0] + '_queen.png'
-		self.img = pygame.image.load(img_path)
-		self.img = pygame.transform.scale(self.img, (board.tile_width - 20, board.tile_height - 20))
+    img_path = 'data/imgs/' + color[0] + '_queen.png'
+    self.img = pygame.image.load(img_path)
+    self.img = pygame.transform.scale(self.img, (board.tile_width - 20, board.tile_height - 20))
 
-		self.notation = 'Q'
+    self.notation = 'Q'
 
+  def getValidMoves(gs: GameState, pos: Tuple[int]) -> Tuple[int]:
+    row, col = pos
+    player_color = "w" if gs.turn.value == 0 else "b"
+    # Check if the piece is valid
+    if gs.board[row][col][0] != player_color or  gs.board[row][col][1] != "Q":
+      return []
 
-	def get_possible_moves(self, board):
-		output = []
+    output = []
+    # Move North
+    moves_north = []
+    for x in range(row)[::-1]:
+      square = gs.board[x][col]
+      if square != "":
+        if square[1] != player_color:
+          moves_north.append((x,col))
+        break
+      moves_north.append((x,col))
+    output.append(moves_north)
 
-		moves_north = []
-		for y in range(self.y)[::-1]:
-			moves_north.append(board.get_square_from_pos(
-				(self.x, y)
-			))
-		output.append(moves_north)
+    # Move SouthEast
+    moves_se = []
+    for i in range(1, 8):
+      tar_row, tar_col = (row+i, col+i)
+      if tar_row > 7 or tar_col > 7:
+        break
+      square = gs.board[tar_row][tar_col]
+      if square != "":
+        if square[1] != player_color:
+          moves_se.append((tar_row, tar_col))
+        break
+      moves_se.append((tar_row, tar_col))
+    output.append(moves_se)
 
-		moves_ne = []
-		for i in range(1, 8):
-			if self.x + i > 7 or self.y - i < 0:
-				break
-			moves_ne.append(board.get_square_from_pos(
-				(self.x + i, self.y - i)
-			))
-		output.append(moves_ne)
+    # Move East
+    moves_east = []
+    for x in range(col + 1, 8):
+      square = gs.board[row][x]
+      if square != "":
+        if square[1] != player_color:
+          moves_east.append((row,x))
+        break
+      moves_east.append((row,x))
+    output.append(moves_east)
 
-		moves_east = []
-		for x in range(self.x + 1, 8):
-			moves_east.append(board.get_square_from_pos(
-				(x, self.y)
-			))
-		output.append(moves_east)
+    # Move NorthEast
+    moves_ne = []
+    for i in range(1, 8):
+      tar_row, tar_col = (row-i, col+i)
+      if tar_row < 0 or tar_col > 7:
+        break
+      square = gs.board[tar_row][tar_col]
+      if square != "":
+        if square[1] != player_color:
+          moves_ne.append((tar_row, tar_col))
+        break
+      moves_ne.append((tar_row, tar_col))
+    output.append(moves_ne)
 
-		moves_se = []
-		for i in range(1, 8):
-			if self.x + i > 7 or self.y + i > 7:
-				break
-			moves_se.append(board.get_square_from_pos(
-				(self.x + i, self.y + i)
-			))
-		output.append(moves_se)
+    # Move South
+    moves_south = []
+    for x in range(row + 1, 8):
+      square = gs.board[x][col]
+      if square != "":
+        if square[1] != player_color:
+          moves_south.append((x,col))
+        break
+      moves_south.append((x,col))
+    output.append(moves_south)
 
-		moves_south = []
-		for y in range(self.y + 1, 8):
-			moves_south.append(board.get_square_from_pos(
-				(self.x, y)
-			))
-		output.append(moves_south)
+    # Move NorthWest
+    moves_nw = []
+    for i in range(1, 8):
+      tar_row, tar_col = (row-i, col-i)
+      if tar_row < 0 or tar_col < 0:
+        break
+      square = gs.board[tar_row][tar_col]
+      if square != "":
+        if square[1] != player_color:
+          moves_nw.append((tar_row, tar_col))
+        break
+      moves_nw.append((tar_row, tar_col))
+    output.append(moves_nw)
 
-		moves_sw = []
-		for i in range(1, 8):
-			if self.x - i < 0 or self.y + i > 7:
-				break
-			moves_sw.append(board.get_square_from_pos(
-				(self.x - i, self.y + i)
-			))
-		output.append(moves_sw)
+    # Move West
+    moves_west = []
+    for y in range(col)[::-1]:
+      square = gs.board[row][y]
+      if square != "":
+        if square[1] != player_color:
+          moves_west.append((row,y))
+        break
+      moves_west.append((row,y))
+    output.append(moves_west)
 
-		moves_west = []
-		for x in range(self.x)[::-1]:
-			moves_west.append(board.get_square_from_pos(
-				(x, self.y)
-			))
-		output.append(moves_west)
+    # Move SouthWest
+    moves_sw = []
+    for i in range(1, 8):
+      tar_row, tar_col = (row+i, col-i)
+      if tar_row > 7 or tar_col < 0:
+        break
+      square = gs.board[tar_row][tar_col]
+      if square != "":
+        if square[1] != player_color:
+          moves_sw.append((tar_row, tar_col))
+        break
+      moves_sw.append((tar_row, tar_col))
+    output.append(moves_sw)
 
-		moves_nw = []
-		for i in range(1, 8):
-			if self.x - i < 0 or self.y - i < 0:
-				break
-			moves_nw.append(board.get_square_from_pos(
-				(self.x - i, self.y - i)
-			))
-		output.append(moves_nw)
-
-		return output
+    return output
