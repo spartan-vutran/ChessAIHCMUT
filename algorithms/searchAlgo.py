65bb462606b48989fe9b6e444f498cdd0b8e2fc7
VuJaxon <vu.tran10100305@gmail.com>
2023-08-28 14:25:56 +0700

    Add timing function for agent

--- algorithms/searchAlgo.py
+++ algorithms/searchAlgo.py
@@ -1,6 +1,8 @@
 from logic.attributes import Turn, GameState, Action, Move, QueenPromote, EnterTower
 from .heuristic import Heuristic
 from logic.game import GameController
+from frontend.settings import TIME_IN_TURN
+import time
 import copy
 
 class SearchAlgo:
@@ -31,24 +33,28 @@ class AlphaBetaAlgo(SearchAlgo):
         
         print(self.game.board_to_string(gs.board))
         print(f"Its heuristic value:{eval_value}")
+        start_time = time.time()  
         for move in self.game.actions(gs):
+            if time.time() - start_time >= TIME_IN_TURN:
+              return best_move
             gsCopy = self.game.move(gs, move)
 
             if turn == Turn.BLACK:
-                print(f"===================Check move {count}========================")
-                print(self.game.board_to_string(gsCopy.board))
-                print(f"Current turn: {'White' if gsCopy.turn == Turn.WHITE else 'Black'}")
+                # print(f"===================Check move {count}========================")
+                # print(self.game.board_to_string(gsCopy.board))
+                # print(f"Current turn: {'White' if gsCopy.turn == Turn.WHITE else 'Black'}")
                 score = self.maxValue(gsCopy, self.depth, alpha, beta)
-                print(f"===================Traceback to========================")
-                print(self.game.board_to_string(gsCopy.board))
-                print(f"Its heuristic value:{eval_value}")
-                print(f"Current best move:{best_move}")
+                
                 count += 1
                 if (score < best_score): 
                     best_score = score
                     best_move = move
                     beta = min(beta, score)
-                    
+                # print(f"===================Traceback to========================")
+                # print(self.game.board_to_string(gsCopy.board))
+                # print(f"Its heuristic value:{score}")
+                # print(f"Current best score:{best_score}")
+                # print(f"Current best move: {best_move.pos} {best_move.tar}")
                     
             else: 
                 score = self.minValue(gsCopy, self.depth, alpha, beta)
@@ -69,19 +75,19 @@ class AlphaBetaAlgo(SearchAlgo):
         count = 1
         for move in self.game.actions(gs):
             newGs = self.game.move(gs, move)
-            print(f"===================Check move {count}========================")
-            print(self.game.board_to_string(newGs.board))
-            print(f"maxValue")
-            print(f"Current turn: {'White' if newGs.turn == Turn.WHITE else 'Black'}")
+            # print(f"===================Check move {count}========================")
+            # print(self.game.board_to_string(newGs.board))
+            # print(f"maxValue")
+            # print(f"Current turn: {'White' if newGs.turn == Turn.WHITE else 'Black'}")
 
             score = self.minValue(newGs, depth-1, alpha, beta)
             best_score = max(best_score, score)
             alpha = max(alpha, score)
-            print(f"===================Traceback to========================")
-            print(self.game.board_to_string(newGs.board))
-            print(f"Its heuristic value:{score}")
-            print(f"Current best score:{best_score}")
-            print(f"Alpha: {alpha} \t Beta: {beta}")
+            # print(f"===================Traceback to========================")
+            # print(self.game.board_to_string(newGs.board))
+            # print(f"Its heuristic value:{score}")
+            # print(f"Current best score:{best_score}")
+            # print(f"Alpha: {alpha} \t Beta: {beta}")
             count += 1
             if beta <= alpha:
                 break  # Beta cutoff
@@ -98,18 +104,18 @@ class AlphaBetaAlgo(SearchAlgo):
         count = 1
         for move in self.game.actions(gs):
             newGs = self.game.move(gs, move)
-            print(f"===================Check move {count}========================")
-            print(self.game.board_to_string(newGs.board))
-            print(f"minValue")
-            print(f"Current turn: {'White' if newGs.turn == Turn.WHITE else 'Black'}")
+            # print(f"===================Check move {count}========================")
+            # print(self.game.board_to_string(newGs.board))
+            # print(f"minValue")
+            # print(f"Current turn: {'White' if newGs.turn == Turn.WHITE else 'Black'}")
             score = self.maxValue(newGs, depth-1, alpha, beta)
             best_score = min(best_score, score)
             beta = min(beta, score)
-            print(f"===================Traceback to========================")
-            print(self.game.board_to_string(newGs.board))
-            print(f"Its heuristic value:{score}")
-            print(f"Current best score:{best_score}")
-            print(f"Alpha: {alpha} \t Beta: {beta}")
+            # print(f"===================Traceback to========================")
+            # print(self.game.board_to_string(newGs.board))
+            # print(f"Its heuristic value:{score}")
+            # print(f"Current best score:{best_score}")
+            # print(f"Alpha: {alpha} \t Beta: {beta}")
             count += 1
             if beta <= alpha:
                 break  # Alpha cutoff
@@ -129,7 +135,7 @@ class MinMaxAlgo(SearchAlgo):
         best_move = None
         turn = gs.turn
         best_score = MinMaxAlgo.INFINITE if turn == Turn.BLACK else -MinMaxAlgo.INFINITE
-        
+        start_time = time.time()
         for move in self.game.actions(gs):
             gsCopy = copy.deepcopy(gs)
             if not self.game.checkValidMove(gsCopy, move):
@@ -152,6 +158,9 @@ class MinMaxAlgo(SearchAlgo):
                     best_score = score
                     best_move = move
             print(score)
+
+            if time.time() - start_time >= TIME_IN_TURN:
+              return best_move
 
         return best_move
     
